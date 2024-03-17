d37e185621f05cea9c0780a06a9d615b2162d4ea
Raphael-Pauleschi <Raphael.f.pauleschi@gmail.com>
2024-03-17 11:11:04 -0300

    Project Init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.get("/", function(req, res){
+    res.render("primeira_pagina")
+})
+
+app.get("/listar", function(req,res){
+    res.render("listar")
+})
+
+app.listen(8081, function(){
+    console.log("Servidor Ativo!")
+})
